9a8a3dc86ba995dd9897a2e7866e56bd9169c5a9
crasmu75 <crasmu75@bruinmail.slcc.edu>
2014-04-20 01:13:46 -0600

    writing some new classes

---
+++ Apples To Apples/Apples To Apples/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apples_To_Apples
+{
+    class Player
+    {
+        private int playerNum; //we will need to update this once we figure out how to give each player a number
+    }
+}
