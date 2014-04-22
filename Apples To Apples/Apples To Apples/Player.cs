6bc15c622f779c7e007d9171a4437505793cc880
crasmu75 <crasmu75@bruinmail.slcc.edu>
2014-04-21 19:36:02 -0600

    testing

--- Apples To Apples/Apples To Apples/Player.cs
+++ Apples To Apples/Apples To Apples/Player.cs
@@ -10,7 +10,7 @@ namespace Apples_To_Apples
     {
         public int playerNum; //we will need to update this once we figure out how many players are connected
         public int awesomePoints = 0;
-        public Boolean isJudge = true;
+        public Boolean isJudge = false;
 
         public Player(int number)
         {
