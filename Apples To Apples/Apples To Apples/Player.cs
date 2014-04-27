6bb1e855b7cea061afc4d1e23930892fc894c310
crasmu75 <crasmu75@bruinmail.slcc.edu>
2014-04-27 17:18:52 -0600

    adding player getters and setters

--- Apples To Apples/Apples To Apples/Player.cs
+++ Apples To Apples/Apples To Apples/Player.cs
@@ -9,12 +9,22 @@ namespace Apples_To_Apples
     class Player
     {
         public int playerNum; //we will need to figure out how to retrieve this num from website
-        public int awesomePoints = 0;
+        public int awesomePts = 0;
         public Boolean isJudge = false;
 
         public Player(int number)
         {
             playerNum = number;
+        }
+
+        public int getAwesomePts()
+        {
+            return awesomePts;
+        }
+
+        public void setAwesomePts(int newPts)
+        {
+            awesomePts = newPts;
         }
     }
 }
